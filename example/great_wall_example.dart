f3eee0ff95992d514f06e843445d1b3bebfb819f
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-05-22 15:58:54 +0300

    Initial package commit and hierarchy structuring.

---
+++ example/great_wall_example.dart
@@ -0,0 +1,6 @@
+import 'package:great_wall/great_wall.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/great_wall.dart
@@ -0,0 +1,13 @@
+/// A core implementation of the Great-Wall protocol.
+///
+/// Great-Wall is a protocol for providing Kerckhoffian, 0-trust, deviceless
+/// coercion-resistance in self-custody. See the protocol doc for more details.
+///
+/// ```dart
+/// GreatWallProtocol great_wall = GreatWallProtocol();
+/// ```
+library;
+
+export 'src/great_wall_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.

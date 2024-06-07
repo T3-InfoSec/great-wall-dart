5126ea554b493d9074ae51f4f45175cdefcbb76c
flatFeather <github.astronaut143@silomails.com>
2024-06-07 11:13:12 +0200

    Add unit test for Greatwall class current implementation

--- example/great_wall_example.dart
+++ example/great_wall_example.dart
@@ -1,6 +1,6 @@
 import 'package:great_wall/great_wall.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  late GreatWall greatWall = GreatWall()
+  print('greatwall current level: ${greatWall.currentLevel}');
 }

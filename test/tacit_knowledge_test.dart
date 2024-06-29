d10c0a42bcc01818e54b96cb9799ee61c77a91d7
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-06-29 03:55:01 +0300

    Add tests for tacit knowledge params.

--- test/tacit_knowledge_test.dart
+++ test/tacit_knowledge_test.dart
@@ -7,7 +7,9 @@ import 'package:test/test.dart';
 void main() {
   group('Tacit knowledge params tests', () {
     late FormosaTacitKnowledgeParam formosaTacitKnowledgeParam;
-    late FractalTacitKnowledgeParam fractalTacitKnowledgeParam;
+    late FractalTacitKnowledgeParam realFractalTacitKnowledgeParam;
+    late FractalTacitKnowledgeParam imaginaryFractalTacitKnowledgeParam;
+    late FractalTacitKnowledgeParam generalFractalTacitKnowledgeParam;
     // late HashVizTacitKnowledgeParam hashvizTacitKnowledgeParam;
 
     setUp(() {
@@ -17,25 +19,80 @@ void main() {
         Uint8List.fromList([0]),
       );
 
-      fractalTacitKnowledgeParam = FractalTacitKnowledgeParam(
+      realFractalTacitKnowledgeParam = FractalTacitKnowledgeParam(
         "realParam",
         Uint8List(128),
-        Uint8List.fromList([1, 2, 3, 4]),
+        Uint8List.fromList([1, 2, 3]),
+      );
+
+      imaginaryFractalTacitKnowledgeParam = FractalTacitKnowledgeParam(
+        "imaginaryParam",
+        Uint8List(128),
+        Uint8List.fromList([1, 2, 3]),
+      );
+
+      generalFractalTacitKnowledgeParam = FractalTacitKnowledgeParam(
+        "generalParam",
+        Uint8List(128),
+        Uint8List.fromList([1, 2, 3]),
       );
     });
 
-    test('Constructor', () {
+    test('Formosa Param Constructor', () {
+      expect(formosaTacitKnowledgeParam.name, 'arityIdx');
+      expect(formosaTacitKnowledgeParam.initialState, Uint8List(128));
+      expect(
+        formosaTacitKnowledgeParam.adjustmentValue,
+        Uint8List.fromList([0]),
+      );
+    });
+
+    test('Fractal Param Constructor', () {
+      expect(realFractalTacitKnowledgeParam.name, 'realParam');
+      expect(realFractalTacitKnowledgeParam.initialState, Uint8List(128));
+      expect(
+        realFractalTacitKnowledgeParam.adjustmentValue,
+        Uint8List.fromList([1, 2, 3]),
+      );
+
+      expect(imaginaryFractalTacitKnowledgeParam.name, 'imaginaryParam');
+      expect(imaginaryFractalTacitKnowledgeParam.initialState, Uint8List(128));
+      expect(
+        imaginaryFractalTacitKnowledgeParam.adjustmentValue,
+        Uint8List.fromList([1, 2, 3]),
+      );
+
+      expect(generalFractalTacitKnowledgeParam.name, 'generalParam');
+      expect(generalFractalTacitKnowledgeParam.initialState, Uint8List(128));
+      expect(
+        generalFractalTacitKnowledgeParam.adjustmentValue,
+        Uint8List.fromList([1, 2, 3]),
+      );
+    });
+
+    test('Formosa Param value', () {
+      expect(
+        formosaTacitKnowledgeParam.value,
+        Uint8List.fromList([57, 116, 180, 27]),
+      );
+    });
+
+    test('Fractal Param value', () {
+      expect(realFractalTacitKnowledgeParam.value, 2.993161161);
+      expect(imaginaryFractalTacitKnowledgeParam.value, 0.993161161);
+      expect(generalFractalTacitKnowledgeParam.value, 2.993161161);
     });
   });
 
   group('Tacit knowledge tests', () {
-    late FormosaTacitKnowledge formosaTacitKnowledge;
-    late FractalTacitKnowledge fractalTacitKnowledge;
-    // late HashVizTacitKnowledge hashvizTacitKnowledge;
-
-    late DerivationPath derivationPath;
-
-    setUp(() {
-    });
+    // late FormosaTacitKnowledge formosaTacitKnowledge;
+    // late FractalTacitKnowledge fractalTacitKnowledge;
+    // // late HashVizTacitKnowledge hashvizTacitKnowledge;
+    //
+    // late DerivationPath derivationPath;
+    //
+    // setUp(() {
+    //   derivationPath = DerivationPath(nodesList: [1, 2, 3]);
+    // });
   });
 }

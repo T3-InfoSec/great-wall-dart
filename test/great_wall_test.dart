97f7833597cea178e5ae7bbe4fac48b337d5fbfa
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-07-01 17:40:17 +0300

    Add protocol 'generateKnowledgePalettes' tests.

--- test/great_wall_test.dart
+++ test/great_wall_test.dart
@@ -44,11 +44,11 @@ void main() {
     test('Starting protocol derivation', () {
       greatwallProtocol.startDerivation();
 
-      print(greatwallProtocol.currentState);
       expect(greatwallProtocol.isCanceled, isFalse);
       expect(greatwallProtocol.isStarted, isTrue);
       expect(greatwallProtocol.isFinished, isFalse);
       expect(greatwallProtocol.isInitialized, isTrue);
+      expect(greatwallProtocol.derivationResult, isNull);
       expect(greatwallProtocol.currentState, [
         137, 143, 186, 18, 167, 0, 209, 173, 77, 196, 218, 89, 207, 51, 250,
         144, 152, 162, 136, 237, 157, 39, 32, 136, 188, 239, 171, 44, 4, 129,
@@ -60,7 +60,33 @@ void main() {
         117, 102, 99, 248, 36, 144, 144, 148, 92, 54, 7, 249, 82, 80, 79, 251,
         41, 211, 155, 130, 154, 192, // Current state of the derivation.
       ]);
+    });
+
+    test('Generate knowledge palettes', () {
+      greatwallProtocol.startDerivation();
+      List<TacitKnowledge> knowledgePalettes =
+          greatwallProtocol.generateKnowledgePalettes();
+      expect(knowledgePalettes.length, greatwallProtocol.treeArity);
+      expect(knowledgePalettes[0], isA<FormosaTacitKnowledge>());
+      expect(knowledgePalettes[1], isA<FormosaTacitKnowledge>());
+      expect(knowledgePalettes[2], isA<FormosaTacitKnowledge>());
+
+      expect(greatwallProtocol.isCanceled, isFalse);
+      expect(greatwallProtocol.isStarted, isTrue);
+      expect(greatwallProtocol.isFinished, isFalse);
+      expect(greatwallProtocol.isInitialized, isTrue);
       expect(greatwallProtocol.derivationResult, isNull);
+      expect(greatwallProtocol.currentState, [
+        137, 143, 186, 18, 167, 0, 209, 173, 77, 196, 218, 89, 207, 51, 250,
+        144, 152, 162, 136, 237, 157, 39, 32, 136, 188, 239, 171, 44, 4, 129,
+        22, 25, 119, 115, 153, 235, 115, 5, 16, 107, 83, 193, 136, 106, 32,
+        62, 235, 145, 77, 240, 24, 243, 196, 50, 94, 85, 40, 102, 187, 15, 207,
+        234, 215, 131, 53, 56, 190, 180, 183, 54, 6, 65, 197, 185, 138, 98,
+        129, 173, 37, 216, 76, 249, 255, 129, 67, 74, 128, 190, 248, 38, 111,
+        163, 255, 123, 247, 131, 26, 48, 130, 202, 116, 216, 183, 71, 101, 94,
+        117, 102, 99, 248, 36, 144, 144, 148, 92, 54, 7, 249, 82, 80, 79, 251,
+        41, 211, 155, 130, 154, 192, // Current state of the derivation.
+      ]);
     });
   });
 }

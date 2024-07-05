10db46f18414ec311301fec70168c38d081e6b4e
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-07-05 18:12:29 +0300

    Add example.

--- example/great_wall_example.dart
+++ example/great_wall_example.dart
@@ -1,6 +1,34 @@
 import 'package:great_wall/great_wall.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  GreatWall greatwallProtocol = GreatWall(
+    treeArity: 3,
+    treeDepth: 5,
+    timeLockPuzzleParam: 10,
+  );
+
+  greatwallProtocol.startDerivation();
+
+  print(greatwallProtocol.currentLevelKnowledgePalettes);
+  greatwallProtocol.tacitDerivation(
+    choiceNumber: 1,
+  ); // Choose the first palette
+  print(greatwallProtocol.currentLevelKnowledgePalettes);
+  greatwallProtocol.tacitDerivation(
+    choiceNumber: 2,
+  ); // Choose the second palette
+  print(greatwallProtocol.currentLevelKnowledgePalettes);
+  greatwallProtocol.tacitDerivation(
+    choiceNumber: 3,
+  ); // Choose the third palette
+  print(greatwallProtocol.currentLevelKnowledgePalettes);
+  greatwallProtocol.tacitDerivation(
+    choiceNumber: 1,
+  );
+  print(greatwallProtocol.currentLevelKnowledgePalettes);
+  greatwallProtocol.tacitDerivation(
+    choiceNumber: 2,
+  );
+
+  greatwallProtocol.finishDerivation();
 }
